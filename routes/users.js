4919d71bce8d5ce56b7030b5fb20aa5ae98897df
Jintosy <fuji0731@gmail.com>
2023-08-03 21:57:59 +0900

    ユーザ情報の更新実装

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,30 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("user router");
-})
+// ユーザ取得の更新
+router.put("/:id", async (req, res) => {
+  // ユーザ自体か管理者であれば更新可能とする
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("ユーザ情報が更新されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("自分のアカウントのみ更新可能です");
+  }
+});
+
+
+
+// ユーザ情報の削除
+
+
+// ユーザ情報の取得
+
 
 
 module.exports = router;
