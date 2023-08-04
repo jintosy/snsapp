bb56ef41e417ac21290036dd07f5f672a3b41de7
Jintosy <fuji0731@gmail.com>
2023-08-04 22:05:31 +0900

    add:投稿作成機能実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("posts router");
-})
+
+// 投稿を作成する
+// api/posts/
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 
 
 module.exports = router;
