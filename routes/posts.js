1b5465b7bce6c4ae839dfc03b429c26b5e65cb41
Jintosy <fuji0731@gmail.com>
2023-08-07 22:43:51 +0900

    feature:タイムライン機能実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
-
+const User = require("../models/User");
 
 // 投稿を作成する
 // api/posts/
@@ -95,6 +95,25 @@ router.put("/:id/like", async (req, res) => {
       });
       return res.status(200).json("いいねを取り消しました");
     }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+
+// タイムラインの投稿を取得する
+// /api/posts/timeline/all
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    // 自分がフォローしているユーザの投稿内容を全て取得する
+    const followPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    return res.status(200).json(userPosts.concat(...followPosts));
   } catch (err) {
     return res.status(500).json(err);
   }
