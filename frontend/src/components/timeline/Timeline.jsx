d439d458dd176329c5e9b9abe752a5844542828f
Jintosy <fuji0731@gmail.com>
2023-08-16 22:02:13 +0900

    fix:タイムラインコンポーネントに投稿入力と投稿一覧を追加

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import "./Timeline.css";
+import Share from '../share/Share';
+import Post from '../post/Post';
 
 export default function Timeline() {
-  return <div className="timeline">Timeline</div>
+  return <div className="timeline">
+    <div className="timelineWrapper">
+      <Share />
+      <Post />
+    </div>
+  </div>
 }
