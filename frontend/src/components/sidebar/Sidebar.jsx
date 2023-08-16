49de4f6b5e18d2718c62aad89624bb3f3524e643
Jintosy <fuji0731@gmail.com>
2023-08-16 20:52:01 +0900

    add:サイドバーコンポーネントの雛形作成

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,66 @@
+import { Bookmark, Home, MessageRounded, Notifications, Person, Search, Settings } from '@mui/icons-material'
 import React from 'react'
+import "./Sidebar.css";
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Home className="sidebarIcon" />
+            <span className="sidebarListItemText">ホーム</span>
+          </li>
+          <li className="sidebarListItem">
+            <Search className="sidebarIcon" />
+            <span className="sidebarListItemText">検索</span>
+          </li>
+          <li className="sidebarListItem">
+            <Notifications className="sidebarIcon" />
+            <span className="sidebarListItemText">通知</span>
+          </li>
+          <li className="sidebarListItem">
+            <MessageRounded className="sidebarIcon" />
+            <span className="sidebarListItemText">メッセージ</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">ブックマーク</span>
+          </li>
+          <li className="sidebarListItem">
+            <Person className="sidebarIcon" />
+            <span className="sidebarListItemText">プロフィール</span>
+          </li>
+          <li className="sidebarListItem">
+            <Settings className="sidebarIcon" />
+            <span className="sidebarListItemText">設定</span>
+          </li>
+        </ul>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="sidebarFriendImg" />
+            <span className="sidebarFriendName">John Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/3.jpeg"
+              alt=""
+              className="sidebarFriendImg" />
+            <span className="sidebarFriendName">John Doe2</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/4.jpeg"
+              alt=""
+              className="sidebarFriendImg" />
+            <span className="sidebarFriendName">John Doe3</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
