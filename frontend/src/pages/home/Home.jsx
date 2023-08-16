402ef86428f592f70646bb09a831c252a6d068d7
Jintosy <fuji0731@gmail.com>
2023-08-16 20:16:30 +0900

    fix:作成するコンポーネントのファイル作成して読み込むように修正

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Topbar from '../../components/topbar/Topbar';
-
+import Sidebar from '../../components/sidebar/Sidebar';
+import Timeline from '../../components/timeline/Timeline';
+import Rightbar from '../../components/rightbar/Rightbar';
 
 export default function Home() {
   return (
     <>
-      {/* Topbar */}
       <Topbar />
-      {/* SideBar */}
-      {/* Timeline */}
-      {/* RightBar */}
+      <Sidebar />
+      <Timeline />
+      <Rightbar />
     </>
   );
 }
